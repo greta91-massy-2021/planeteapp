92002564120ff3e1986b56ebb48ebed6e9298237
Soupramanien BOUVANESVARY <soupramanien@baobab-ingenierie.fr>
2021-03-05 15:20:57 +0100

    Final version

--- app/src/main/java/fr/baobab/planeteapp/adapter/PlaneteAdapter.java
+++ app/src/main/java/fr/baobab/planeteapp/adapter/PlaneteAdapter.java
@@ -1,17 +1,33 @@
 package fr.baobab.planeteapp.adapter;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.List;
 
 import fr.baobab.planeteapp.model.Planete;
+import fr.baobab.planeteapp.view.PlaneteView;
 
 public class PlaneteAdapter extends ArrayAdapter<Planete> {
-
+    private List<Planete> list;
     public PlaneteAdapter(@NonNull Context context, @NonNull List<Planete> planetes) {
         super(context, 0, planetes);
+        list = planetes;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        PlaneteView planeteView = (PlaneteView)convertView;
+        if(null == planeteView){
+            planeteView = PlaneteView.create(parent);
+        }
+        planeteView.setItem(this.list.get(position));
+        return planeteView;
     }
 }
---
+++ app/src/main/java/fr/baobab/planeteapp/view/PlaneteView.java
@@ -0,0 +1,42 @@
+package fr.baobab.planeteapp.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import fr.baobab.planeteapp.R;
+import fr.baobab.planeteapp.model.Planete;
+
+public class PlaneteView extends RelativeLayout {
+    private TextView tvName;
+    private TextView tvDistance;
+    private ImageView ivImage;
+    public PlaneteView(Context context, AttributeSet attrs){
+        super(context, attrs);
+    }
+
+    public static PlaneteView create(ViewGroup parent){
+        //expanser la vue
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        PlaneteView planeteView = (PlaneteView) inflater.inflate(R.layout.item, parent, false);
+        //initialiser les variables d'instance de cet objet palneteView
+        planeteView.findViews();
+        return planeteView;
+    }
+
+    private void findViews() {
+        this.tvName = findViewById(R.id.nomPlanete);
+        this.tvDistance = findViewById(R.id.distancePlanete);
+        this.ivImage = findViewById(R.id.imagePlanete);
+    }
+
+    public void setItem(final Planete planete) {
+        this.tvName.setText(planete.getNom());
+        this.tvDistance.setText(planete.getDistance()+"Gm");
+        this.ivImage.setImageResource(planete.getIdImage());
+    }
+}
